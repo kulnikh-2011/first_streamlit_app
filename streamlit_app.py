93ab760285a1cb034907f5e27dd585a77cfba9d4
Nikhil Kulkarni <109883619+kulnikh-2011@users.noreply.github.com>
2022-07-24 17:58:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🍇 🥣 Omega 3 and Blueberry oatmeal')
 streamlit.text('🥦 🥬 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled free range eggs')
 streamlit.text('🥑 🍞 Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
