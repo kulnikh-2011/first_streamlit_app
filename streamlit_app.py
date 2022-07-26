4374219c8626f53867e5743f97809706dd249117
Nikhil Kulkarni <109883619+kulnikh-2011@users.noreply.github.com>
2022-07-26 16:17:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,7 @@ streamlit.write('The user added '+new_fruit+' fruit')
 
 def insert_row_snowflake(new_fruit):
   new_fruit = streamlit.text_input('Which fruit would you like to add')
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor as my_cur:
     my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')")
     return "Thanks fro adding "+ new_fruit
